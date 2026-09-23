132284e90e89c15c24bebe2e6c4ce7b01ae9e219
Student Dylan Barry (TL_KCOMP_B_Y4) <t00252785@mymtu.ie>
2026-09-23 10:51:17 +0100

    Signed-off-by: Student Dylan Barry (TL_KCOMP_B_Y4) <t00252785@mymtu.ie>

--- PhysicsProject/Assets/Scripts/SpherePhysics.cs
+++ PhysicsProject/Assets/Scripts/SpherePhysics.cs
@@ -1,9 +1,12 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AdaptivePerformance;
+using UnityEngine.UI;
 
 public class SpherePhysics : MonoBehaviour
 {
     public PlanePhysics thePlane;
+    float d0;
     public float Radius 
        { get { return transform.localScale.x / 2f; }
         set { transform.localScale = Vector3.one * value;}
@@ -26,22 +29,78 @@ public class SpherePhysics : MonoBehaviour
         // v = u + a * t
         // velocity = velocity + acceleration * Time.deltaTime; 
 
+        Vector3 oldVelocity = velocity;
+        Vector3 oldPosition = transform.position;
+
         velocity += acceleration * Time.deltaTime;
 
         // s = u * t
 
         transform.position += velocity * Time.deltaTime;
 
-        // transform.position += perp_component(velocity, thePlane.Normal) * Time.deltaTime;
+        float d1 = parallel_Distance(transform.position - thePlane.transform.position, thePlane.Normal) - Radius;
+        d0 = d1;
 
-        if (parallel_Distance(transform.position - thePlane.transform.position,
-                             thePlane.Normal) < Radius) // detect collision
+        if (d1 < 0) // detect collision
         {
             Vector3 parallelComponent = parallel_component(velocity, thePlane.Normal);
             Vector3 perpendicularComponent = perp_component(velocity, thePlane.Normal);
-            velocity = perpendicularComponent - (parallelComponent * CoR);
-            transform.position -= parallelComponent * Time.deltaTime;
+            //velocity = perpendicularComponent - (parallelComponent * CoR);
+            //transform.position -= parallelComponent * Time.deltaTime;
+
+            // V drop = d1 - d0 divided by time.deltatime
+
+            float vDrop = d1 - d0 / Time.deltaTime;
+            // T impact = -d0 divided by drop
+            float tImpact = -d0 / vDrop;
+            // V impact = old velocity + acceleration * T impact
+            // P impact = P0 + V impact + T impact
+            Vector3 vImpact = oldVelocity + acceleration * tImpact;
+
+            Vector3 pImpact = oldPosition + vImpact * tImpact;
+
+            // Resolve collision
+            // vImpactOut = v perp - CoR * v parallel
+            Vector3 vImpactOut = perp_component(velocity, thePlane.Normal) - (CoR * parallel_component(velocity, thePlane.Normal));
+
+            // Fast Forward to current frame 
+            // T remaining = T - tImpact
+            float tRemaining = Time.deltaTime - tImpact;
+
+            // v = vImpactOut + acceleration * tRemaining (v = vector/velocity?)
+            Vector3 v = vImpactOut + acceleration * tRemaining;
+            // P = pImpact + velocity * tRemaining
+            Vector3 position = pImpact + v * tRemaining;
+
+            velocity = v;
+            transform.position = position;
         }
+
+        //d0 = d1;
+
+        //// V drop = d1 - d0 divided by time.deltatime
+
+        //float vDrop = d1 - d0 / Time.deltaTime;
+        //// T impact = -d0 divided by drop
+        //float tImpact = -d0 / vDrop;
+        //// V impact = old velocity + acceleration * T impact
+        //// P impact = P0 + V impact + T impact
+        //Vector3 vImpact = oldVelocity + acceleration * tImpact;
+
+        //Vector3 pImpact = oldPosition + vImpact * tImpact;
+
+        //// Resolve collision
+        //// vImpactOut = v perp - CoR * v parallel
+        //Vector3 vImpactOut = perp_component(velocity, thePlane.Normal) - (CoR * parallel_component(velocity, thePlane.Normal));
+
+        //// Fast Forward to current frame 
+        //// T remaining = T - tImpact
+        //float tRemaining = Time.deltaTime - tImpact;
+
+        //// v = vImpactOut + acceleration * tRemaining (v = vector/velocity?)
+        //Vector3 v = vImpactOut + acceleration * tRemaining;
+        //// P = pImpact + velocity * tRemaining
+        //Vector3 position = pImpact + v * tRemaining;
     }
 
     // Returns the magnitude of the parallel component of vector v parallel  to n
